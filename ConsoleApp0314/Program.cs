54feb7d58fa7e695e825d9b102c03833d7382f23
user <user@DESKTOP-6JBEF5K>
2022-03-14 09:58:55 +0800

    test1

--- ConsoleApp0314/Program.cs
+++ ConsoleApp0314/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp0314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("請輸入一數字:");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if(i%2 != 0)
+            {
+                Console.WriteLine("這是奇數");
+            }
+            else
+            {
+                Console.WriteLine("這是偶數");
+            }
         }
     }
 }
