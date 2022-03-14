fc0868d0ea3b60bc5c436fcb72975ae08a193e3a
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:14:33 +0800

    test1

--- ConsoleApp0314/Program.cs
+++ ConsoleApp0314/Program.cs
@@ -8,13 +8,21 @@ namespace ConsoleApp0314
         {
             Console.WriteLine("請輸入一數字:");
             int i = Convert.ToInt32(Console.ReadLine());
-            if(i%2 != 0)
+            if (i % 2 != 0  )
             {
-                Console.WriteLine("這是奇數");
+                Console.WriteLine("Weird");
+            }
+            else if(i / 2 >=2 && i/2<=5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (i / 2 >= 6 && i / 2 <= 20)
+            {
+                Console.WriteLine("Weird");
             }
             else
             {
-                Console.WriteLine("這是偶數");
+                Console.WriteLine("Not Weird");
             }
         }
     }
