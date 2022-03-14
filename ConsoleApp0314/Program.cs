038ea9e40d4e5db53e79bf034c6c55c0065320e1
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:07:45 +0800

    practice2

--- ConsoleApp0314/Program.cs
+++ ConsoleApp0314/Program.cs
@@ -6,24 +6,54 @@ namespace ConsoleApp0314
     {
         static void Main(string[] args)
         {
+            //練習一
             Console.WriteLine("請輸入一數字:");
-            int i = Convert.ToInt32(Console.ReadLine());
-            if (i % 2 != 0  )
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int f = 1; f <= n; f++)
             {
-                Console.WriteLine("Weird");
+                for (int i = 0; i < f; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
-            else if(i / 2 >=2 && i/2<=5)
+            //練習二
+            Console.WriteLine("請輸入一數字:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= n2; i++)
             {
-                Console.WriteLine("Not Weird");
+                Console.WriteLine(n2+"*"+i+"="+n2*i);
             }
-            else if (i / 2 >= 6 && i / 2 <= 20)
+            //練習三
+            for(int i = 1; i <= 9; i++)
             {
-                Console.WriteLine("Weird");
-            }
-            else
-            {
-                Console.WriteLine("Not Weird");
+                for(int f = 1; f <= 9; f++)
+                {
+                    Console.Write(i+"*"+f+"="+i*f+" ");
+                }
+                Console.WriteLine();
             }
         }
+
+
+        /*Console.WriteLine("請輸入一數字:");
+        int i = Convert.ToInt32(Console.ReadLine());
+        if (i % 2 != 0  )
+        {
+            Console.WriteLine("Weird");
+        }
+        else if(i / 2 >=2 && i/2<=5)
+        {
+            Console.WriteLine("Not Weird");
+        }
+        else if (i / 2 >= 6 && i / 2 <= 20)
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+            Console.WriteLine("Not Weird");
+        }*/
     }
 }
+
